191bd752c30ecdaedc6a6069a60a3c3ac46707e7
Bogdawaa <bogdan6246@yandex.ru>
2025-07-08 12:34:09 +0300

    Initial Commit

---
+++ Pixverse/PixverseApp.swift
@@ -0,0 +1,17 @@
+//
+//  PixverseApp.swift
+//  Pixverse
+//
+//  Created by Bogdan Fartdinov on 08.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct PixverseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
