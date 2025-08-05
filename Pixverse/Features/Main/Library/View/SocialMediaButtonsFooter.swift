a13b0f636b05d6e9c41f957176aca6a6a9ed7a12
Bogdawaa <bogdan6246@yandex.ru>
2025-08-05 14:05:29 +0300

    добавлены sharelinks для кнопок соцсетей

--- Pixverse/Features/Main/Library/View/SocialMediaButtonsFooter.swift
+++ Pixverse/Features/Main/Library/View/SocialMediaButtonsFooter.swift
@@ -13,27 +13,39 @@ struct SocialMediaButtonsFooter: View {
     
     var body: some View {
         HStack(spacing: 12) {
-            SocialMediaButton(
-                icon: Image(.instagram),
-                label: "Instagram"
-            )
-            
-            SocialMediaButton(
-                icon: Image(.facebook),
-                label: "Facebook"
-            )
-            
-            SocialMediaButton(
-                icon: Image(.tikTok),
-                label: "TikTok"
-            )
-            
-            SocialMediaButton(
-                icon: Image(.whatsapp),
-                label: "WhatsApp"
-            )
-            
             if let shareItem = shareItem {
+                ShareLink(item: shareItem) {
+                    SocialMediaButton(
+                        icon: Image(.instagram),
+                        label: "Instagram"
+                    )
+                    .disabled(true) // disabled for sharelink interaction collision
+                }
+                
+                ShareLink(item: shareItem) {
+                    SocialMediaButton(
+                        icon: Image(.facebook),
+                        label: "Facebook"
+                    )
+                    .disabled(true) // disabled for sharelink interaction collision
+                }
+                
+                ShareLink(item: shareItem) {
+                    SocialMediaButton(
+                        icon: Image(.tikTok),
+                        label: "TikTok"
+                    )
+                    .disabled(true) // disabled for sharelink interaction collision
+                }
+                
+                ShareLink(item: shareItem) {
+                    SocialMediaButton(
+                        icon: Image(.whatsapp),
+                        label: "WhatsApp"
+                    )
+                    .disabled(true) // disabled for sharelink interaction collision
+                }
+                
                 ShareLink(item: shareItem) {
                     SocialMediaButton(
                         icon: Image(systemName: "ellipsis"),
